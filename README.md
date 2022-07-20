bc5218f41eb57573286d5bb0bf3d7d66f75264ed
Keith Butters <keith.butters@gmail.com>
2022-07-20 16:20:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## every-threejs-swift
+
+Quick test of some threejs in swift
---
+++ ThreeJSExample/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ThreeJSExample
+//
+//  Created by Chedi BACCARI on 28/10/2019.
+//  Copyright © 2019 Chedi BACCARI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = ["Load Every Test 1"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSegue",
+            let destinationVC = segue.destination as? DetailsViewController {
+            destinationVC.selectedItem = tableView.indexPathForSelectedRow?.row
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdenitifier", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+}
